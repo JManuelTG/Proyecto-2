03c32b3b41a172cc9fbf8d8451ddfc941b915449
JManuelTG <jose.tarula@alumnos.udg.mx>
2022-10-14 04:04:32 -0500

    Se crea modelo resourse

---
+++ app/Http/Controllers/ServicioController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Servicio;
+use Illuminate\Http\Request;
+
+class ServicioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $servicios = Servicio::all();
+        return view('servicios.servicioIndex', compact('servicios'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Servicio  $servicio
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Servicio $servicio)
+    {
+        $orden = Servicio::find($servicio);
+        return view('servicios.servicioShow', compact('orden'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Servicio  $servicio
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Servicio $servicio)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Servicio  $servicio
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Servicio $servicio)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Servicio  $servicio
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Servicio $servicio)
+    {
+        //
+    }
+}
